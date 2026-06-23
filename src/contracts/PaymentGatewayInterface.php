3d45b32130eabe0362c37a489b298b136cee644b
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2026-06-23 05:48:07 +0300

    use DTOs in request and response

--- src/contracts/PaymentGatewayInterface.php
+++ src/contracts/PaymentGatewayInterface.php
@@ -1,9 +1,12 @@
 <?php
 namespace Hammam\PaymentGateways\contracts;
 
+use Hammam\PaymentGateways\DTOs\PaymentData;
+use Hammam\PaymentGateways\DTOs\PaymentResponse;
+
 interface PaymentGatewayInterface
 {
-    public function pay($data);
+    public function pay(PaymentData $data): PaymentResponse;
     public function refund(string $transactionId, float $amount): bool;
     public function verify($data): bool;
 }
